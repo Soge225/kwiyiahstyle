27bebc703e3bcf75ce0b64cdfd70a56a20bea25e
www <sogexia.fr@gmail.com>
2025-05-11 22:36:17 +0000

    Add files via upload

---
+++ redirectnewsletters.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+if (isset($_POST['submit'])) {
+    $mail = filter_var($_POST['mail'], FILTER_SANITIZE_EMAIL);
+
+    if (!empty($mail) && filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        $message = "Bonjour, vous avez reçu un nouvel abonnement à la newsletter :\n\nAdresse mail de l'abonné : $mail";
+        $headers = "From: noreply@kwiyiah.com\r\n";
+        mail("Kwiyiah@gmail.com", "✅ NOUVEL ABONNEMENT À LA NEWSLETTER", $message, $headers);
+
+        $_SESSION['newsletter_success'] = true;
+    }
+    header('Location: index.php');
+    exit();
+}
+?>
